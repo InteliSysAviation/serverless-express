db12992263ea904ffceaee0904e69562a4d12594
Kristian Luck <kluck@intelisysaviation.com>
2025-05-07 18:25:53 +0000

    fix: corred alb implementation
    Associated JIRA issue is SOAR-2328.
    Add unit tests to illustrate the the ALB processing isn't correct with respect to the CodeGenie codebase from which this was forked.
    Update the implementation of alb.js so the tests pass.
    Change the name of the package so InteliSys can use it independently from @codegenie

--- __tests__/unit.alb.js
+++ __tests__/unit.alb.js
@@ -1,21 +1,106 @@
 const eventSources = require('../src/event-sources')
-const testUtils = require('../src/event-sources/utils.test')
+const testUtils = require('./utils')
 
 const apiGatewayEventSource = eventSources.getEventSource({
-  eventSourceName: 'AWS_API_GATEWAY_V2'
+  eventSourceName: 'AWS_ALB'
 })
 
-test('request has correct headers', () => {
-  const req = getReq()
+test('GET request has correct headers when multi-valued headers are not enabled', () => {
+  const request = apiGatewayEventSource.getRequest({ event: testUtils.albGetWithoutMvhEvent })
   // see https://github.com/vendia/serverless-express/issues/387
-  expect(typeof req).toEqual('object')
-  expect(JSON.stringify(req.headers)).toEqual(
-    '{"cookie":"","host":"localhost:9000","user-agent":"curl/7.64.1","accept":"*/*","x-forwarded-proto":"http","x-forwarded-port":"9000"}'
+  expect(typeof request).toEqual('object')
+  expect(JSON.stringify(request.headers)).toEqual(
+    '{"accept":"*/*","accept-encoding":"gzip, deflate, br","connection":"keep-alive","host":"echoevent.intelisysqa.ca",' +
+'"postman-token":"0ca3fb83-0b91-4e47-bb03-359746d144b4","user-agent":"PostmanRuntime/7.28.0",' +
+'"x-amzn-trace-id":"Root=1-60c06484-136f10932a9d72ea34407239","x-forwarded-for":"14.169.54.234",' +
+'"x-forwarded-port":"443","x-forwarded-proto":"https","x-multivalue-header":"veg,celery","content-length":0}'
   )
 })
 
-function getReq () {
-  const event = testUtils.sam_httpapi_event
-  const request = apiGatewayEventSource.getRequest({ event })
-  return request
-}
+test('GET request has correct headers when multi-valued headers are enabled', () => {
+  const request = apiGatewayEventSource.getRequest({ event: testUtils.albGetWithMvhEvent })
+  // see https://github.com/vendia/serverless-express/issues/387
+  expect(typeof request).toEqual('object')
+  expect(JSON.stringify(request.headers)).toEqual(
+    '{"accept":"*/*","accept-encoding":"gzip, deflate, br","connection":"keep-alive","host":"echoevent.intelisysqa.ca",' +
+'"postman-token":"f1231e8c-4929-4b91-b8b7-a818a4b1a98a","user-agent":"PostmanRuntime/7.28.0",' +
+'"x-amzn-trace-id":"Root=1-60c0630d-67397f4609b8e19d5ca10427","x-forwarded-for":"14.169.54.234",' +
+'"x-forwarded-port":"443","x-forwarded-proto":"https","x-multivalue-header":"veg,pea,veg,celery","content-length":0}'
+  )
+})
+
+test('POST request has correct headers when multi-valued headers are not enabled', () => {
+  const request = apiGatewayEventSource.getRequest({ event: testUtils.albPostWithoutMvhEvent })
+  // see https://github.com/vendia/serverless-express/issues/387
+  expect(typeof request).toEqual('object')
+  expect(JSON.stringify(request.headers)).toEqual(
+    '{"accept":"*/*","accept-encoding":"gzip, deflate, br","connection":"keep-alive","content-length":425,' +
+    '"content-type":"multipart/form-data; boundary=--------------------------053813846626992301385195",' +
+    '"host":"echoevent.intelisysqa.ca","postman-token":"49429809-039d-4171-a920-db2f04feb390",' +
+    '"user-agent":"PostmanRuntime/7.28.0","x-amzn-trace-id":"Root=1-60c06465-2549927316296af552839b7d",' +
+    '"x-forwarded-for":"14.169.54.234","x-forwarded-port":"443","x-forwarded-proto":"https",' +
+    '"x-multivalue-header":"veg,celery"}'
+  )
+})
+
+test('POST request has correct headers when multi-valued headers are enabled', () => {
+  const request = apiGatewayEventSource.getRequest({ event: testUtils.albPostWithMvhEvent })
+  // see https://github.com/vendia/serverless-express/issues/387
+  expect(typeof request).toEqual('object')
+  expect(JSON.stringify(request.headers)).toEqual(
+    '{"accept":"*/*","accept-encoding":"gzip, deflate, br","connection":"keep-alive","content-length":425,' +
+    '"content-type":"multipart/form-data; boundary=--------------------------790755205523996327761243",' +
+    '"host":"echoevent.intelisysqa.ca","postman-token":"da3ce04c-d67c-44a6-b707-c83755334b1d",' +
+    '"user-agent":"PostmanRuntime/7.28.0","x-amzn-trace-id":"Root=1-60c063d6-338c9f07762d7142793c4ae5",' +
+    '"x-forwarded-for":"14.169.54.234","x-forwarded-port":"443","x-forwarded-proto":"https",' +
+    '"x-multivalue-header":"veg,pea,veg,celery"}'
+  )
+})
+
+describe('get remote IP address', () => {
+  test('from an ELB event with multi value headers disabled', () => {
+    const request = apiGatewayEventSource.getRequest({ event: testUtils.albGetWithoutMvhEvent })
+    expect(request.remoteAddress).toBe('14.169.54.234')
+  })
+  test('from an ELB event with multi value headers enabled', () => {
+    const request = apiGatewayEventSource.getRequest({ event: testUtils.albGetWithMvhEvent })
+    expect(request.remoteAddress).toBe('14.169.54.234')
+  })
+})
+
+describe('get raw querystring', () => {
+  test('from an ELB event with multi value headers disabled and no querystring parameters', () => {
+    const event = {
+      ...testUtils.albGetWithoutMvhEvent,
+      ...{
+        queryStringParameters: {}
+      }
+    }
+    const request = apiGatewayEventSource.getRequest({ event })
+    const expectedPath = '/pp%C2%A5pp'
+    expect(request.path).toBe(expectedPath)
+  })
+  test('from an ELB event with multi value headers disabled', () => {
+    const event = testUtils.albGetWithoutMvhEvent
+    const request = apiGatewayEventSource.getRequest({ event })
+    const expectedPath = '/pp%C2%A5pp?multi%C2%A5Value=red%2C+green&test%C2%A5Param=Hello+World%3A+%C2%A5'
+    expect(request.path).toBe(expectedPath)
+  })
+  test('from an ELB event with multi value headers enabled', () => {
+    const event = testUtils.albGetWithMvhEvent
+    const request = apiGatewayEventSource.getRequest({ event })
+    const expectedPath = '/pp%C2%A5pp?multi%C2%A5Value=red+%C2%A5+apple&multi%C2%A5Value=red%2C+green&test%C2%A5Param=Hello+World%3A+%C2%A5'
+    expect(request.path).toBe(expectedPath)
+  })
+  test('from an ELB event with multi value headers enabled and no querystring parameters', () => {
+    const event = {
+      ...testUtils.albGetWithMvhEvent,
+      ...{
+        multiValueQueryStringParameters: {}
+      }
+    }
+    const request = apiGatewayEventSource.getRequest({ event })
+    const expectedPath = '/pp%C2%A5pp' // /pp¥pp
+    expect(request.path).toBe(expectedPath)
+  })
+})
--- __tests__/utils.js
+++ __tests__/utils.js
@@ -267,6 +267,219 @@ const selfManagedKafkaEvent = {
   }
 }
 
+/**
+ * The following ALB sample events were based on a hypothetical form input with the following shape:
+ *
+  <form action="https://albhostname.company.tld/pp¥pp" method="get">
+    <input type="text" name="test¥Param" value="Hello World: ¥" />
+    <input type="hidden" name="multi¥Value" value="red ¥ apple" />
+    <input type="hidden" name="multi¥Value" value="red, green" />
+    <input type="submit" />
+  </form>
+ *
+ * The actual HTTP requests were performed with Postman to prevent browser-specific headers from
+ * being submitted. In addition to the above form data, two values for a multivalue header named
+ * 'x-multivalue-header' were sumitted to capture how that translates into the ALB event. Here are
+ * the two values submitted for for this header: ['veg,pea' 'veg,celery']. The comma was deliberately
+ * used in each of these to see how the ELB dealt with it.
+ *
+ * Some important characteristics that were being exercised here are:
+ *   - the URL contains non-ascii unicode characters
+ *   - the form parameter names and values contain non-ascii unicode characters
+ *   - there is a multi-value form parameter
+ *   - there is a multi-value header
+ */
+
+/**
+This is an event captured from an ELB event source. Specifically, this was captured from a target group
+forwarding a GET HTTP request to a lambda function with the "Multi value headers" attribute NOT enabled.
+Some interesting observations:
+  - only one of the multi-value parameter values is forwarded by the ELB to the lambda event
+  - only one of the multi-value header values is forwarded by the ELB to the lambda event
+*/
+const albGetWithoutMvhEvent = {
+  requestContext: {
+    elb: {
+      targetGroupArn: 'arn:aws:elasticloadbalancing:ap-southeast-1:880230544159:targetgroup/lambda-echoevent-tg/09a2a46d2acfba96'
+    }
+  },
+  httpMethod: 'GET',
+  path: '/pp%C2%A5pp',
+  queryStringParameters: {
+    'multi%C2%A5Value': 'red%2C+green',
+    'test%C2%A5Param': 'Hello+World%3A+%C2%A5'
+  },
+  headers: {
+    accept: '*/*',
+    'accept-encoding': 'gzip, deflate, br',
+    connection: 'keep-alive',
+    host: 'echoevent.intelisysqa.ca',
+    'postman-token': '0ca3fb83-0b91-4e47-bb03-359746d144b4',
+    'user-agent': 'PostmanRuntime/7.28.0',
+    'x-amzn-trace-id': 'Root=1-60c06484-136f10932a9d72ea34407239',
+    'x-forwarded-for': '14.169.54.234',
+    'x-forwarded-port': '443',
+    'x-forwarded-proto': 'https',
+    'x-multivalue-header': 'veg,celery'
+  },
+  body: '',
+  isBase64Encoded: false
+}
+
+/**
+This is an event captured from an ELB event source. Specifically, this was captured from a target group
+forwarding a GET HTTP request to a lambda function with the "Multi value headers" attribute NOT enabled.
+*/
+const albGetWithMvhEvent = {
+  requestContext: {
+    elb: {
+      targetGroupArn: 'arn:aws:elasticloadbalancing:ap-southeast-1:880230544159:targetgroup/lambda-echoevent-tg/09a2a46d2acfba96'
+    }
+  },
+  httpMethod: 'GET',
+  path: '/pp%C2%A5pp',
+  multiValueQueryStringParameters: {
+    'multi%C2%A5Value': [
+      'red+%C2%A5+apple',
+      'red%2C+green'
+    ],
+    'test%C2%A5Param': [
+      'Hello+World%3A+%C2%A5'
+    ]
+  },
+  multiValueHeaders: {
+    accept: [
+      '*/*'
+    ],
+    'accept-encoding': [
+      'gzip, deflate, br'
+    ],
+    connection: [
+      'keep-alive'
+    ],
+    host: [
+      'echoevent.intelisysqa.ca'
+    ],
+    'postman-token': [
+      'f1231e8c-4929-4b91-b8b7-a818a4b1a98a'
+    ],
+    'user-agent': [
+      'PostmanRuntime/7.28.0'
+    ],
+    'x-amzn-trace-id': [
+      'Root=1-60c0630d-67397f4609b8e19d5ca10427'
+    ],
+    'x-forwarded-for': [
+      '14.169.54.234'
+    ],
+    'x-forwarded-port': [
+      '443'
+    ],
+    'x-forwarded-proto': [
+      'https'
+    ],
+    'x-multivalue-header': [
+      'veg,pea',
+      'veg,celery'
+    ]
+  },
+  body: '',
+  isBase64Encoded: false
+}
+
+/**
+This is an event captured from an ELB event source. Specifically, this was captured from a target group
+forwarding a GET HTTP request to a lambda function with the "Multi value headers" attribute NOT enabled.
+Some interesting observations:
+  - only one of the multi-value header values is forwarded by the ELB to the lambda event
+*/
+const albPostWithoutMvhEvent = {
+  requestContext: {
+    elb: {
+      targetGroupArn: 'arn:aws:elasticloadbalancing:ap-southeast-1:880230544159:targetgroup/lambda-echoevent-tg/09a2a46d2acfba96'
+    }
+  },
+  httpMethod: 'POST',
+  path: '/pp%C2%A5pp',
+  queryStringParameters: {},
+  headers: {
+    accept: '*/*',
+    'accept-encoding': 'gzip, deflate, br',
+    connection: 'keep-alive',
+    'content-length': '425',
+    'content-type': 'multipart/form-data; boundary=--------------------------053813846626992301385195',
+    host: 'echoevent.intelisysqa.ca',
+    'postman-token': '49429809-039d-4171-a920-db2f04feb390',
+    'user-agent': 'PostmanRuntime/7.28.0',
+    'x-amzn-trace-id': 'Root=1-60c06465-2549927316296af552839b7d',
+    'x-forwarded-for': '14.169.54.234',
+    'x-forwarded-port': '443',
+    'x-forwarded-proto': 'https',
+    'x-multivalue-header': 'veg,celery'
+  },
+  body: 'LS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLTA1MzgxMzg0NjYyNjk5MjMwMTM4NTE5NQ0KQ29udGVudC1EaXNwb3NpdGlvbjogZm9ybS1kYXRhOyBuYW1lPSJ0ZXN0wqVQYXJhbSINCg0KSGVsbG8gV29ybGQ6IMKlDQotLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tMDUzODEzODQ2NjI2OTkyMzAxMzg1MTk1DQpDb250ZW50LURpc3Bvc2l0aW9uOiBmb3JtLWRhdGE7IG5hbWU9Im11bHRpwqVWYWx1ZSINCg0KcmVkIMKlIGFwcGxlDQotLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tMDUzODEzODQ2NjI2OTkyMzAxMzg1MTk1DQpDb250ZW50LURpc3Bvc2l0aW9uOiBmb3JtLWRhdGE7IG5hbWU9Im11bHRpwqVWYWx1ZSINCg0KcmVkLCBncmVlbg0KLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLTA1MzgxMzg0NjYyNjk5MjMwMTM4NTE5NS0tDQo=',
+  isBase64Encoded: true
+}
+
+/**
+This is an event captured from an ELB event source. Specifically, this was captured from a target group
+forwarding a POST HTTP request to a lambda function with the "Multi value headers" attribute NOT enabled.
+*/
+const albPostWithMvhEvent = {
+  requestContext: {
+    elb: {
+      targetGroupArn: 'arn:aws:elasticloadbalancing:ap-southeast-1:880230544159:targetgroup/lambda-echoevent-tg/09a2a46d2acfba96'
+    }
+  },
+  httpMethod: 'POST',
+  path: '/pp%C2%A5pp',
+  multiValueQueryStringParameters: {},
+  multiValueHeaders: {
+    accept: [
+      '*/*'
+    ],
+    'accept-encoding': [
+      'gzip, deflate, br'
+    ],
+    connection: [
+      'keep-alive'
+    ],
+    'content-length': [
+      '425'
+    ],
+    'content-type': [
+      'multipart/form-data; boundary=--------------------------790755205523996327761243'
+    ],
+    host: [
+      'echoevent.intelisysqa.ca'
+    ],
+    'postman-token': [
+      'da3ce04c-d67c-44a6-b707-c83755334b1d'
+    ],
+    'user-agent': [
+      'PostmanRuntime/7.28.0'
+    ],
+    'x-amzn-trace-id': [
+      'Root=1-60c063d6-338c9f07762d7142793c4ae5'
+    ],
+    'x-forwarded-for': [
+      '14.169.54.234'
+    ],
+    'x-forwarded-port': [
+      '443'
+    ],
+    'x-forwarded-proto': [
+      'https'
+    ],
+    'x-multivalue-header': [
+      'veg,pea',
+      'veg,celery'
+    ]
+  },
+  body: 'LS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLTc5MDc1NTIwNTUyMzk5NjMyNzc2MTI0Mw0KQ29udGVudC1EaXNwb3NpdGlvbjogZm9ybS1kYXRhOyBuYW1lPSJ0ZXN0wqVQYXJhbSINCg0KSGVsbG8gV29ybGQ6IMKlDQotLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tNzkwNzU1MjA1NTIzOTk2MzI3NzYxMjQzDQpDb250ZW50LURpc3Bvc2l0aW9uOiBmb3JtLWRhdGE7IG5hbWU9Im11bHRpwqVWYWx1ZSINCg0KcmVkIMKlIGFwcGxlDQotLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tNzkwNzU1MjA1NTIzOTk2MzI3NzYxMjQzDQpDb250ZW50LURpc3Bvc2l0aW9uOiBmb3JtLWRhdGE7IG5hbWU9Im11bHRpwqVWYWx1ZSINCg0KcmVkLCBncmVlbg0KLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLTc5MDc1NTIwNTUyMzk5NjMyNzc2MTI0My0tDQo=',
+  isBase64Encoded: true
+}
+
 describe('getEventSourceNameBasedOnEvent', () => {
   test('throws error on empty event', () => {
     expect(() => getEventSourceNameBasedOnEvent({ event: {} })).toThrow(
@@ -318,6 +531,26 @@ describe('getEventSourceNameBasedOnEvent', () => {
     const result = getEventSourceNameBasedOnEvent({ event: eventbridgeCustomerEvent })
     expect(result).toEqual('AWS_EVENTBRIDGE')
   })
+
+  test('recognizes ELB event for GET HTTP request without multi value headers enabled', () => {
+    const result = getEventSourceNameBasedOnEvent({ event: albGetWithoutMvhEvent })
+    expect(result).toEqual('AWS_ALB')
+  })
+
+  test('recognizes ELB event for POST HTTP request without multi value headers enabled', () => {
+    const result = getEventSourceNameBasedOnEvent({ event: albPostWithoutMvhEvent })
+    expect(result).toEqual('AWS_ALB')
+  })
+
+  test('recognizes ELB event for GET HTTP request with multi value headers enabled', () => {
+    const result = getEventSourceNameBasedOnEvent({ event: albGetWithMvhEvent })
+    expect(result).toEqual('AWS_ALB')
+  })
+
+  test('recognizes ELB event for POST HTTP request with multi value headers enabled', () => {
+    const result = getEventSourceNameBasedOnEvent({ event: albPostWithMvhEvent })
+    expect(result).toEqual('AWS_ALB')
+  })
 })
 
 module.exports = {
@@ -329,5 +562,9 @@ module.exports = {
   eventbridgeScheduledEvent,
   eventbridgeCustomerEvent,
   kinesisDataStreamEvent,
-  selfManagedKafkaEvent
+  selfManagedKafkaEvent,
+  albGetWithoutMvhEvent,
+  albGetWithMvhEvent,
+  albPostWithoutMvhEvent,
+  albPostWithMvhEvent
 }
